59ffabc5bcaba4ef682ce58d0488aff479beab8f
Jesse1224 <jessekartabani@gmail.com>
2022-10-31 16:59:29 +1100

    Complete logic

--- components/ColorGuess.js
+++ components/ColorGuess.js
@@ -1,12 +1,15 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import React, { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const ColorGuess = () => {
   // Track if users answer is correct
-  const [correctAnswer, setCorrectAnswer] = useState([false]);
-  // State of color
+  const [correctAnswer, setCorrectAnswer] = useState(["false"]);
+  // State of colors
   const [color, setColor] = useState([]);
+  const [fakeColor, setFakeColor] = useState([]);
+  const [fakeColor1, setFakeColor1] = useState([]);
+  const [answers, setAnswers] = useState([]);
 
   // Generate random number
   function randomNum(min, max) {
@@ -14,16 +17,42 @@ const ColorGuess = () => {
   }
 
   // Generate random color
-  function randomColor() {
+  function randomColors(state) {
     const r = randomNum(0, 255);
     const g = randomNum(0, 255);
     const b = randomNum(0, 255);
-    setColor(`rgb(${r},${g},${b})`);
+    state(`rgb(${r},${g},${b})`);
   }
 
+  // Check if user gets correct answer
+  function checkAnswer(answer) {
+    // If correct randomize colors again
+    if (answer == color) {
+      setCorrectAnswer("true");
+      randomColors(setColor);
+      randomColors(setFakeColor);
+      randomColors(setFakeColor1);
+    } else {
+      setCorrectAnswer("false");
+    }
+  }
+  // Set random colors on startup
   useEffect(() => {
-    randomColor();
+    randomColors(setColor);
+    randomColors(setFakeColor);
+    randomColors(setFakeColor1);
   }, []);
+
+  // Randomly sort colors in array each time new ones are generated
+  useEffect(() => {
+    // Sorted into random order
+    setAnswers([color, fakeColor, fakeColor1].sort(() => 0.5 - Math.random()));
+  }, [color]);
+
+  //console.log(color);
+  //console.log(fakeColor);
+  //console.log(fakeColor1);
+  //console.log(answers);
 
   return (
     <SafeAreaView>
@@ -36,10 +65,44 @@ const ColorGuess = () => {
           alignSelf: "center",
         }}
       ></View>
+
+      {/* Display text if user gets answer correct */}
+      <View>{correctAnswer == "true" && <Text>Correct</Text>}</View>
+
+      <View styles={styles.answers}>
+        <TouchableOpacity
+          onPress={async () => {
+            checkAnswer(answers[0]);
+          }}
+        >
+          <Text>{answers[0]}</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={async () => {
+            checkAnswer(answers[1]);
+          }}
+        >
+          <Text>{answers[1]}</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={async () => {
+            checkAnswer(answers[2]);
+          }}
+        >
+          <Text>{answers[2]}</Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
 
 export default ColorGuess;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  answers: {
+    width: 100,
+    height: 100,
+  },
+});
