0a037fe82672debbd421182423936b26b461ab17
Jesse1224 <jessekartabani@gmail.com>
2022-11-01 15:06:39 +1100

    Add background image

--- App.js
+++ App.js
@@ -1,15 +1,25 @@
-import { StyleSheet } from "react-native";
+import { ImageBackground, StyleSheet } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import ColorGuess from "./components/ColorGuess";
 import Heading from "./components/Heading";
 
 export default function App() {
   return (
-    <SafeAreaProvider style={styles.appContainer}>
-      <Heading />
-      <ColorGuess />
-    </SafeAreaProvider>
+    <ImageBackground
+      style={styles.backgroundImg}
+      source={require("./assets/background.jpg")}
+    >
+      <SafeAreaProvider style={styles.appContainer}>
+        <Heading />
+        <ColorGuess />
+      </SafeAreaProvider>
+    </ImageBackground>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  backgroundImg: {
+    width: "100%",
+    height: "100%",
+  },
+});
